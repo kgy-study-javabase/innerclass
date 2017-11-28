8553f92445a9e4326ee866e47cc70efbf103966b
kouguangyuan <2275738566@qq.com>
2017-11-28 15:48:55 +0800

    init

---
+++ src/com/innerclass/test/LocalInnerClasses.java
@@ -0,0 +1,23 @@
+package com.innerclass.test;
+
+public class LocalInnerClasses {
+
+}
+class People{
+    public People() {
+         
+    }
+}
+ 
+class Man{
+    public Man(){
+         
+    }
+     
+    public People getWoman(){
+        class Woman extends People{   //局部内部类
+            int age =0;
+        }
+        return new Woman();
+    }
+}
